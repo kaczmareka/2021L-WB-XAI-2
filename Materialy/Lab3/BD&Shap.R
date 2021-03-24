4767fa52f83ac40fa85f9c130b68ab37f5453aba
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2021-03-24 10:54:19 +0100

    init

--- Materialy/Lab3/BD&Shap.R
+++ Materialy/Lab3/BD&Shap.R
@@ -51,7 +51,7 @@ classif_lrn <- makeLearner("classif.svm", predict.type = "prob")
 model_mlr <- train(classif_lrn, classif_task)
 
 explainer_mlr <- explain(model = model_mlr,
-                         data = titanic_imputed_fct,
+                         data = titanic_imputed_fct[,-8],
                          y = as.numeric(as.character(titanic_imputed_fct$survived)))
 
 # Widzimy, ¿e mlr tez jest domyslnie wspierany
@@ -62,7 +62,7 @@ pp_ranger_bd_1 <- predict_parts(explainer, new_observation = titanic_imputed[1,]
                                 order = c("gender", "age", "class", "embarked", "fare", "sibsp", "parch"))
 plot(pp_ranger_bd_1)
 
-pp_ranger_bd_2 <- predict_parts(explainer, new_observation = titanic_imputed[13,])
+pp_ranger_bd_2 <- predict_parts(explainer_mlr, new_observation = titanic_imputed[13,])
 plot(pp_ranger_bd_2)
 
 
